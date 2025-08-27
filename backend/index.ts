8f7f86c234edf06f2c50167bdfaab53958924e13
bruno-henrique-crtl <bruno.henriqueds@outlook.com.br>
2025-08-26 22:31:41 -0300

    adicionando prettier e eslint

--- backend/index.ts
+++ backend/index.ts
@@ -12,8 +12,8 @@ app.use(express.json());
 require("./server")(app);
 
 const server = http.createServer(app);
-require("./socket")(server); 
+require("./socket")(server);
 
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
     console.log("Servidor rodando na porta 3000");
 });
